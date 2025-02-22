a28372c9b2e6320fd794e28878b2513099c1a0ce
Sanzhar Beishekeev <franki-55@mail.ru>
2025-02-22 17:56:36 +0600

    [Exam-12] [UI] - в главном компоненте для карточек добавил кнопку удаления только для админа, а в галереи пользователя кнопка удаления появляется только для пользователя или для админа, если в галереи пользователя нет картинок и он не является владельцем то его выкидывает в главное меню, также кнопка добавить фото появляется для владельца галереи, изменил отображение имени владельца галереи на более правильный вариант через find

--- frontend/src/features/photos/container/Photos.tsx
+++ frontend/src/features/photos/container/Photos.tsx
@@ -1,14 +1,16 @@
 import {Card, CardMedia, CardContent, Typography, Modal} from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import {useAppDispatch, useAppSelector} from "../../../app/hooks.ts";
-import {selectLoading, selectOnePhoto, selectPhotos} from "../photoSlice.ts";
+import {selectDeleting, selectLoading, selectOnePhoto, selectPhotos} from "../photoSlice.ts";
 import {useEffect, useState} from "react";
-import {getOnePhoto, getPhotoThunks} from "../photoThunks.ts";
+import {deletePhoto, getOnePhoto, getPhotoThunks} from "../photoThunks.ts";
 import Spinner from "../../../components/UI/Spinner/Spinner.tsx";
 import {apiUrl} from "../../../globalConstants.ts";
 import {NavLink} from "react-router-dom";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import {toast} from "react-toastify";
+import {selectUser} from "../../users/userSlice.ts";
 
 
 const Photos = () => {
@@ -17,6 +19,9 @@ const Photos = () => {
     const onePhoto = useAppSelector(selectOnePhoto);
     const loading = useAppSelector(selectLoading);
     const [openModal, setOpenModal] = useState<boolean>(false);
+    const [selectDelete, setSelectDelete] = useState<string>('');
+    const isDelete = useAppSelector(selectDeleting);
+    const user = useAppSelector(selectUser);
 
     useEffect(() => {
         dispatch(getPhotoThunks())
@@ -30,6 +35,18 @@ const Photos = () => {
     const closeModal = () => {
         setOpenModal(false);
     };
+
+    const onDelete = async (photoId: string) => {
+        setSelectDelete(photoId);
+        try {
+            await dispatch(deletePhoto(photoId)).unwrap();
+            await dispatch(getPhotoThunks())
+            toast.success("Product deleted successfully.");
+            setSelectDelete('');
+        } catch (error) {
+            toast.error((error as { error: string }).error);
+        }
+    }
 
     const image = onePhoto?.image
         ? apiUrl + "/" + onePhoto.image
@@ -47,15 +64,18 @@ const Photos = () => {
                             ? apiUrl + "/" + photo.image
                             : "https://mui.com/static/images/cards/contemplative-reptile.jpg";
                         return (
-                            <Grid key={photo._id}>
+                            <Grid key={photo._id} sx={{flexWrap: "wrap"}}>
                                     <Card sx={{
                                         boxShadow: 3,
                                         borderRadius: 3,
                                         width: "250px",
+                                        height: '100%',
                                         transition: "0.3s",
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        flexGrow: 1,
                                         "&:hover": { transform: "scale(1.05)" }
                                     }}
-                                          onClick={() => {openModalHandler(photo._id)}}
                                     >
                                         <CardMedia
                                             component="img"
@@ -63,8 +83,9 @@ const Photos = () => {
                                             width="100%"
                                             height="300px"
                                             image={image}
+                                            onClick={() => {openModalHandler(photo._id)}}
                                         />
-                                        <CardContent sx={{ textAlign: "center" }}>
+                                        <CardContent sx={{ textAlign: "center", flexGrow: 1 }}>
                                             <Typography variant="h6" sx={{ fontWeight: "bold" }}>
                                                 {photo.title}
                                             </Typography>
@@ -72,6 +93,28 @@ const Photos = () => {
                                                 By: <NavLink to={`/users-photo/${photo.user._id}`} style={{color: 'black'}}>{photo.user.displayName}</NavLink>
                                             </Typography>
                                         </CardContent>
+                                        {user && user.role === 'admin' && (
+                                            <Button
+                                                sx={{
+                                                    backgroundColor: "transparent",
+                                                    border: "1px solid #07575b",
+                                                    color: "#07575b",
+                                                    marginTop: '5px',
+                                                    width: 'max-content',
+                                                    margin: '0 auto 10px auto',
+                                                    "&:hover": {
+                                                        backgroundColor: "#064f4a",
+                                                        color: "white",
+                                                    },
+                                                }}
+                                                onClick={() => {
+                                                    onDelete(photo._id)
+                                                }}
+                                                disabled={isDelete && selectDelete === photo._id}
+                                            >
+                                                Delete
+                                            </Button>
+                                        )}
                                     </Card>
                             </Grid>
                             )
